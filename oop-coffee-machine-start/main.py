dff4ade45d6b8b6bd316de86c2423a225101eb24
jcostadev23 <carlos_jesus_costa@hotmail.com>
2023-02-21 18:42:35 +0000

    add the report option and the switch off option

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -7,16 +7,13 @@ coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
 while True:
     choice = input(f"Qual a sua escolha? , {menu.get_items()}")
-    if choice == "report":
+    if choice == "off":
+        break
+    elif choice == "report":
         money_machine.report()
-    if choice == "resources":
         coffee_maker.report()
     my_coffee = menu.find_drink(choice)
     if my_coffee is not None:
-        my_coffe_price = my_coffee.cost
         if coffee_maker.is_resource_sufficient(my_coffee):
-            if money_machine.make_payment(my_coffe_price):
+            if money_machine.make_payment(my_coffee.cost):
                 coffee_maker.make_coffee(my_coffee)
-
-coffee_maker.report()
-
